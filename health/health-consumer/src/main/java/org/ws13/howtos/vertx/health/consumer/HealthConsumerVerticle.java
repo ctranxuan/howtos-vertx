7cb2dffe50bd56ba48e7cda81c6aaa9ebb6921cc
ctranxuan <ctranxuan@gmail.com>
2017-06-24 17:17:01 +0200

    chore(): remove unused project

--- health/health-consumer/src/main/java/org/ws13/howtos/vertx/health/consumer/HealthConsumerVerticle.java
+++
@@ -1,7 +0,0 @@
-package org.ws13.howtos.vertx.health.consumer;
-
-/**
- * @author ctranxuan (streamdata.io).
- */
-public class HealthConsumerVerticle {
-}
--- health/health-publisher/src/main/java/org/ws13/howtos/vertx/health/publisher/HealthPublisherVerticle.java
+++
@@ -1,7 +0,0 @@
-package org.ws13.howtos.vertx.health.publisher;
-
-/**
- * @author ctranxuan (streamdata.io).
- */
-public class HealthPublisherConsumer {
-}
